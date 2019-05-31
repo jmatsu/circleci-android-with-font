79c994008f549f21e7c303a1802a1407e25b2611
Jumpei Matsuda <jmatsu.drm@gmail.com>
2019-05-31 14:55:42 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Docker images are based on CircleCI's android images but these can use java.awt.font.
+
+For example, https://github.com/maskarade/gradle-android-ribbonizer-plugin works with these images.
+
+ref: https://github.com/circleci/circleci-images
---
+++ create_dockerfiles.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+require 'fileutils'
+
+MIN_SDK=27
+MAX_SDK=27
+DOCKERFILE_DIR='build'
+
+def for_each_dockerfile(&b)
+  (MIN_SDK..MAX_SDK).each do |api_version|
+    name = "#{DOCKERFILE_DIR}/Dockerfile.#{api_version}"
+    b.call(api_version, name)
+  end
+end
+
+def system!(*cmds)
+  system(*cmds.map(&:to_s)) || abort("#{cmds} failed")
+end
+
+FileUtils.mkdir_p(DOCKERFILE_DIR)
+template = File.read("Dockerfile.erb")
+
+for_each_dockerfile do |api_version, name|
+  File.open(name, 'w') do |f|
+    f.write(ERB.new(template).result(binding))
+  end
+end
+
+for_each_dockerfile do |api_version, name|
+  container="jmatsu/android:api-#{api_version}"
+  system!('docker', 'build', '-t', container, '-f', name, '.')
+  system!('docker', 'push', container)
+end
